fb94db8fdca8b629ecd4ebae6207a37216cfb4ce
koffein4ik <koffein4ik@gmail.com>
2021-04-01 23:37:18 +0300

    Second try

---
+++ back/src/main/java/com/example/intercactivetraining/InteractiveTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.intercactivetraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveTrainingApplication.class, args);
+	}
+
+}
