615ce652b3af7f6094fdd08d8964c2d1194fd442
koffein4ik <koffein4ik@gmail.com>
2021-05-18 23:33:02 +0300

    Some updates 3

--- back/src/main/java/com/example/intercactivetraining/model/ResponseViewModel.java
+++ back/src/main/java/com/example/intercactivetraining/model/ResponseViewModel.java
@@ -1,9 +1,22 @@
 package com.example.intercactivetraining.model;
 
-import lombok.Data;
-
-@Data
 public class ResponseViewModel {
     private boolean isErrorOccurred;
     private String errorMessage;
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public boolean isErrorOccurred() {
+        return isErrorOccurred;
+    }
+
+    public void setErrorOccurred(boolean errorOccurred) {
+        isErrorOccurred = errorOccurred;
+    }
 }
